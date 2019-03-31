821e3e835babfa681504a62e85abc0fa76a23f39
Anton Mykolaienko <anton.mikolaenko@gmail.com>
2019-03-31 16:06:18 +0300

    Create models

---
+++ CreditMarket/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace CreditMarket.Models
+{
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public DbSet<Loan> Loans { get; set; }
+		public DbSet<Order> Orders { get; set; }
+
+		public ApplicationDbContext()
+			: base("DefaultConnection", throwIfV1Schema: false)
+		{
+		}
+
+		public static ApplicationDbContext Create()
+		{
+			return new ApplicationDbContext();
+		}
+	}
+}
---
+++ CreditMarket/Models/Order.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CreditMarket.Models
+{
+	public class Order: Base
+	{
+		[Required]
+		public Loan Loan { get; set; }
+
+		[Required]
+		public double Amount { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string FirstName { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string LastName { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string FathersName { get; set; }
+
+		[Required]
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+		public DateTime DateOfBirth { get; set; }
+
+		[StringLength(50)]
+		public string Email { get; set; }
+
+		[Required]
+		[StringLength(12)]
+		public string PhoneNumber { get; set; }
+
+		[Required]
+		public long INN { get; set; }
+
+		[Required]
+		[StringLength(8)]
+		public string PassportNumber { get; set; }
+
+		[Required]
+		[StringLength(120)]
+		public string PassportGivenByWhom { get; set; }
+
+		[Required]
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+		public DateTime PassportGivenDate { get; set; }
+
+		[Required]
+		public byte[] PassportImages { get; set; }
+
+		[Required]
+		public byte[] INNImages { get; set; }
+
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+		public DateTime CreationDate { get; set; }
+
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+		public DateTime ApprovedDate { get; set; }
+
+		public string OrderStatus { get; set; }
+	}
+}
