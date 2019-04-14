3bfa9c4c249cc344bf355a7e1ed8078ada3f7a29
Svitlana <svetlana.mikolaenko@gmail.com>
2019-04-14 21:34:12 +0300

    Add datepicker

--- CreditMarket/App_Start/BundleConfig.cs
+++ CreditMarket/App_Start/BundleConfig.cs
@@ -8,10 +8,12 @@ namespace CreditMarket
 		// For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
+         
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+						"~/Scripts/jquery-{version}.js",
+                         "~/Scripts/moment.js"));
 
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 						"~/Scripts/jquery.validate*"));
 
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
@@ -20,11 +22,14 @@ namespace CreditMarket
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js"));
+                "~/Scripts/bootstrap-datepicker.js",
+                "~/Scripts/locales/bootstrap-datepicker.uk.min.js",
+                "~/Scripts/bootstrap.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
+                      "~/Content/bootstrap-datepicker.css",
+                      "~/Content/site.css"));
 		}
 	}
 }
