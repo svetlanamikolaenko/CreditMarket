d02253929a8161623e2d2e1306b766991216660b
svetlanamikolaenko <Svetlana.mikolaenko@gmail.com>
2019-05-03 23:34:57 +0300

    Pass Amount to Order View (#13)
    * Tables on the View
    
    * Some changes
    
    * Loan's api and dto
    
    * API for Orders
    
    * Fix edit of loans and orders.
    
    * CreationDate on the View.
    
    * Fix Order's editing.
    
    * Modify script for better table's view + delete action added.
    
    * Range sliders.
    
    * Pass Amount Value to Order View
    
    * Update CreateOrder view

--- CreditMarket/Controllers/HomeController.cs
+++ CreditMarket/Controllers/HomeController.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CreditMarket.Controllers
 {
-	public class HomeController : Controller
+    public class HomeController : Controller
 	{
 		public ActionResult Index()
 		{
 			return View();
 		}
 
-		public ActionResult About()
+        [HttpPost]
+        public ActionResult NavigateToOrder()
+        {
+            return RedirectToAction("Create", "Orders");
+        }
+
+        public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
 
