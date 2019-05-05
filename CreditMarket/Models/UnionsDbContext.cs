1cef191e8588901713187e6fcd642e8947850dc6
Svitlana <svetlana.mikolaenko@gmail.com>
2019-05-05 22:44:17 +0300

    add field cardnumber and migration

---
+++ CreditMarket/Models/UnionsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CreditMarket.Models
+{
+    public class UnionsDbContext: DbContext
+    {
+        public UnionsDbContext() : base("name=UkrainianUnionsDb")
+        {
+           
+        }
+        public DbSet<ApprovedOrder> ApprovedOrders { get; set; }
+    }
+   
+}
