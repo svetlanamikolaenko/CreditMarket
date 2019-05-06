e01a93411f2dfed5624627e8d6fa4a3a8f8328ed
svetlanamikolaenko <Svetlana.mikolaenko@gmail.com>
2019-05-06 21:17:58 +0300

    added new table to database (#22)

--- CreditMarket/Migrations.UkrainianUnions/Configuration.cs
+++ CreditMarket/Migrations.UkrainianUnions/Configuration.cs
@@ -1,18 +1,19 @@
-namespace CreditMarket.Migrations
+namespace CreditMarket.Migrations.UkrainianUnions
 {
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<CreditMarket.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<CreditMarket.Models.UnionsDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Migrations.UkrainianUnions";
         }
 
-        protected override void Seed(CreditMarket.Models.ApplicationDbContext context)
+        protected override void Seed(CreditMarket.Models.UnionsDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
